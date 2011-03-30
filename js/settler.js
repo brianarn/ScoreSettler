23c4974f5e00de52ac6515ccadde80d68596f782
Brian Arnold <brianarn@gmail.com>
2011-03-29 19:01:24 -0600

    Basic setup

---
+++ js/settler.js
@@ -0,0 +1,8 @@
+// settler.js
+// Last modified: 2011-03-29 19:00:55
+//
+// Basically, it's what makes the page work
+
+document.addEventListener('DOMContentLoaded', function(){
+	console.log('DOM Ready!');
+}, false);
