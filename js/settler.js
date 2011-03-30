1ea7269414919c8232bf0b8e5c32ff84e5591374
Brian Arnold <brianarn@gmail.com>
2011-03-29 19:28:05 -0600

    First attempt at getting counts moving - not quite right

--- js/settler.js
+++ js/settler.js
@@ -1,8 +1,46 @@
 // settler.js
-// Last modified: 2011-03-29 19:00:55
+// Last modified: 2011-03-29 19:21:09
 //
 // Basically, it's what makes the page work
 
+// Some utility functions
+function rand(min, max) {
+	// Input cleanup
+	if (!max) {
+		max = min;
+		min = 0;
+	}
+	console.log('min,max',min,max);
+	min = parseInt(min, 10);
+	max = parseInt(max, 10);
+	console.log('min,max',min,max);
+
+	// Sanity check
+	if (isNaN(min)) {
+		throw new Error('rand: Invalid input');
+	}
+
+	return Math.floor(Math.random() * (max + 1)) + min;
+} // rand
+
+// DOM Ready check
 document.addEventListener('DOMContentLoaded', function(){
-	console.log('DOM Ready!');
+	var buttons = document.getElementById('buttons'),
+		stats = document.getElementById('stats'),
+		graph = document.getElementById('graph'),
+		graphNodes = [],
+		li, i;
+
+	for (i = 2; i <= 12; ++i) {
+		li = document.createElement('li');
+		li.innerHTML = i;
+		graphNodes.push(li);
+		graph.appendChild(li);
+	}
+
+	setInterval(function(){
+		graphNodes.forEach(function(node) {
+			node.style.height = (rand(0,10) * 10) + '%';
+		});
+	}, 1000);
 }, false);
